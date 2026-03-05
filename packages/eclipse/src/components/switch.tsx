a8b032d132987c99bc46c9e08ff4682b99b0e810
Carla Goncalves <goncalves@prisma.io>
2026-03-05 16:26:55 +0000

    Update radix from base ui Update stylings remove size prop

--- packages/eclipse/src/components/switch.tsx
+++ packages/eclipse/src/components/switch.tsx
@@ -16,7 +16,7 @@ function Switch({
       data-slot="switch"
       data-size={size}
       className={cn(
-        "peer data-[state=checked]:bg-background-neutral-reverse data-[state=unchecked]:bg-background-neutral-strong focus-visible:border-ring  group/switch inline-flex shrink-0 items-center rounded-circle border-1 border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed data-[size=default]:h-[1.15rem] data-[size=default]:w-8 disabled:outline-stroke-neutral-weak disabled:outline disabled:outline-solid disabled:bg-background-neutral-weak disabled:[&>span]:bg-foreground-neutral-weaker [&>span]:bg-foreground-neutral-reverse",
+        "peer data-[checked]:bg-background-neutral-reverse data-[unchecked]:bg-background-neutral-strong focus-visible:border-ring  group/switch inline-flex shrink-0 items-center rounded-circle border-1 border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed h-[1.15rem] w-8 disabled:outline-stroke-neutral-weak disabled:outline disabled:outline-solid disabled:bg-background-neutral-weak disabled:[&>span]:bg-foreground-neutral-weaker [&>span]:bg-foreground-neutral-reverse",
         className,
       )}
       {...props}
@@ -24,7 +24,7 @@ function Switch({
       <SwitchPrimitive.Thumb
         data-slot="switch-thumb"
         className={cn(
-          "dark:data-[state=checked]:bg-primary-foreground pointer-events-none block rounded-full ring-0 transition-transform group-data-[size=default]/switch:size-3 data-[state=checked]:translate-x-[calc(100%+3px)] data-[state=unchecked]:translate-x-[2px] disabled:bg-foreground-neutral-weaker",
+          "dark:data-[checked]:bg-primary-foreground pointer-events-none block rounded-full ring-0 transition-transform size-3 data-[checked]:translate-x-[calc(100%+3px)] data-[unchecked]:translate-x-[2px] disabled:bg-foreground-neutral-weaker",
         )}
       />
     </SwitchPrimitive.Root>
