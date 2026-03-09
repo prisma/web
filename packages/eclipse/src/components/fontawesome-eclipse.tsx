90966ea433013b6830b71f80a10fae7de8f4578e
Carla Goncalves <goncalves@prisma.io>
2026-03-09 16:06:40 +0000

    Update admonition import

--- packages/eclipse/src/components/fontawesome-eclipse.tsx
+++ packages/eclipse/src/components/fontawesome-eclipse.tsx
@@ -1,4 +1,1 @@
-// Using Font Awesome free CDN (no domain restrictions)
-// TODO: Configure kit domain allowlist at fontawesome.com and switch back to: https://kit.fontawesome.com/c1448b716e.js
-export const FontAwesomeScript =
-  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js";
+export const FontAwesomeScript = "https://kit.fontawesome.com/c1448b716e.js";
