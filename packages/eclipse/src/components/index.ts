a6eeb4f1d0c3eba7579c3630ba8972865c9b4809
Ben Sekulowicz-Barclay <ben@seku.co>
2026-03-12 11:26:21 +0100

    Update Input to use sizing

--- packages/eclipse/src/components/index.ts
+++ packages/eclipse/src/components/index.ts
@@ -103,7 +103,7 @@ export {
   TableCell,
   TableCaption,
 } from "./table";
-export { Input } from "./input";
+export { Input, inputVariants } from "./input";
 
 export { Label } from "./label";
 
