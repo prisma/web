fa2effc334c44940a40563086414051bc17850ab
Aidan McAlister <aidankmcalister@gmail.com>
2026-03-20 14:53:29 -0400

    fix(docs): coderabbit comment to add `data-cookieyes="cookieyes-analytics"`

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -45,6 +45,7 @@ export default function Layout({ children }: { children: ReactNode }) {
           src="https://ingest.promptwatch.com/js/client.min.js"
           strategy="afterInteractive"
           data-project-id="25f18e15-6306-4faa-b5c2-8078804778ac"
+          data-cookieyes="cookieyes-analytics"
         />
       </head>
       <body className="flex flex-col min-h-screen">
@@ -53,6 +54,7 @@ export default function Layout({ children }: { children: ReactNode }) {
           async
           src="https://cdn.tolt.io/tolt.js"
           data-tolt="fda67739-7ed0-42d2-b716-6da0edbec191"
+          data-cookieyes="cookieyes-analytics"
         />
         <Script
           async
