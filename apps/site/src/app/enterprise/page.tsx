d37c6ebd6953f3e3acba7c4c2d306f23c2a71138
Carla Goncalves <goncalves@prisma.io>
2026-03-27 05:15:57 +0000

    Add first grid section

--- apps/site/src/app/enterprise/page.tsx
+++ apps/site/src/app/enterprise/page.tsx
@@ -1,3 +1,4 @@
+import LogoParade from "@/components/logo-parade";
 import type { Metadata } from "next";
 import {
   SITE_HOME_DESCRIPTION,
@@ -5,7 +6,6 @@ import {
 } from "../../lib/blog-metadata";
 import { Button, Card, Action } from "@prisma/eclipse";
 import { CopyCode } from "@/components/homepage/copy-btn";
-import LogoParade from "@prisma-docs/ui/components/logo-parade";
 import React from "react";
 import { Bento } from "@/components/homepage/bento";
 import { CardSection } from "@/components/homepage/card-section/card-section";
@@ -13,16 +13,28 @@ import review from "../../data/homepage.json";
 import Testimonials from "../../components/homepage/testimonials";
 import { cn } from "@/lib/cn";
 import { Technology } from "@/components/technology";
-const twoCol = [
+import { FitHeight } from "@rive-app/react-canvas";
+import { Animation } from "@/components/animation";
+
+const first = [
   {
     content: (
       <>
         <h2 className="text-foreground-neutral stretch-display text-4xl font-black! font-sans-display mt-0 mb-4">
-          Postgres that <br /> fits your stack.
+          Boost your <br />
+          application’s lifecycle
         </h2>
-        <p className="text-foreground-neutral-weak! text-base">
-          Works with your existing stack, wherever you deploy.Your choice of
-          ORM, frameworks, and tools, they all just connect.
+        <p className="text-foreground-neutral-weak! text-base my-4">
+          By integrating Prisma into your development ecosystem, you leverage
+          its capabilities to Build robust, adaptable applications with less
+          code and fewer errors and also Fortify your database interactions for
+          peak performance right from the start.
+        </p>
+        <p className="text-foreground-neutral-weak! text-base my-4">
+          As your application Grows, our platform products Accelerate and Prisma
+          Postgres ensure that your data layer can adapt and scale, supporting
+          increased traffic and requirements without sacrificing performance or
+          security.
         </p>
       </>
     ),
@@ -31,31 +43,23 @@ const twoCol = [
     mobileImageUrl: null,
     mobileImageAlt: null,
     logos: null,
-    useDefaultLogos: true,
-    visualPosition: "right" as const,
-    visualType: "logoGrid" as const,
-  },
-  {
-    content: (
-      <>
-        <h2 className="text-foreground-neutral stretch-display text-4xl font-black! font-sans-display mt-0 mb-4">
-          Real Postgres. <br /> Better experience.
-        </h2>
-        <p className="text-foreground-neutral-weak! text-base">
-          The PostgreSQL millions know and trust in production, ready in seconds
-          with zero configuration. Automatic backups, observability and
-          compliance.
-        </p>
-      </>
+    other: (
+      <Animation
+        name="enterprise/build_fortify_grow"
+        fit={FitHeight}
+        className={cn(
+          "h-94",
+          "[&>canvas]:max-w-full",
+          "[&>canvas]:h-auto!",
+          "[&>div]:flex",
+          "[&>div]:items-center",
+          "[&>div]:justify-center",
+        )}
+      />
     ),
-    imageUrl: "/illustrations/homepage/real_ppg",
-    imageAlt: "Real Postgres",
-    mobileImageUrl: "/illustrations/homepage/real_ppg_mobile",
-    mobileImageAlt: "Real PPG mobile",
-    logos: null,
-    useDefaultLogos: false,
+    useDefaultLogos: true,
     visualPosition: "left" as const,
-    visualType: "image" as const,
+    visualType: "other" as const,
   },
 ];
 const databases = {
@@ -150,6 +154,30 @@ const frameworks = {
     },
   ],
 };
+
+const complexities = [
+  {
+    title: "Improved developer experience",
+    image: "/illustrations/enterprise/enterprise_0",
+    subtitle:
+      "Prisma ORM enhances code clarity and modularity. New team members can onboard quickly, thanks to the high level of abstraction and the intuitive query syntax.",
+    icon: "fa-regular fa-cubes-stacked",
+  },
+  {
+    title: "Increased productivity",
+    image: "/illustrations/enterprise/enterprise_1",
+    subtitle:
+      "The Prisma ORM Client API comes with an intuitive querying interface and editor auto-completion, allowing developers to focus on business logic instead of database syntax.",
+    icon: "fa-regular fa-code",
+  },
+  {
+    title: "Bring your own database",
+    subtitle:
+      "Prisma ORM’s extensive compatibility enables teams to work with different databases and switch without significant changes to the application logic.",
+    icon: "fa-regular fa-database",
+  },
+];
+
 export const metadata: Metadata = {
   title: SITE_HOME_TITLE,
   description: SITE_HOME_DESCRIPTION,
@@ -158,106 +186,121 @@ export const metadata: Metadata = {
 export default function SiteHome() {
   return (
     <main className="flex-1 w-full z-1 bg-background-default">
-      <div className="hero h-123 -mt-24 flex items-end justify-center px-4">
-        <div className="content relative z-2 my-12 flex flex-col gap-8">
-          <h1 className="text-6xl [font-variation-settings:'wght'_900,'wdth'_125] mb-0 text-center mt-0 font-sans-display text-foreground-neutral">
-            Postgres, <br />
-            perfectly managed.
-          </h1>
+      <div className="hero -mt-24 flex items-end justify-center px-4 relative">
+        <div className="absolute inset-0 z-0 bg-[linear-gradient(180deg,var(--color-foreground-orm)_0%,var(--color-background-default)_100%)] opacity-20" />
+        <div className="content pt-31 relative z-2 my-12 flex flex-col gap-8">
+          <div className="flex flex-col gap-1">
+            <h5 className="text-foreground-orm-strong text-center stretch-display font-sans-display text-base uppercase">
+              Enterprise & Solution Providers
+            </h5>
+            <h1 className="text-6xl [font-variation-settings:'wght'_900,'wdth'_125] mb-0 text-center mt-0 font-sans-display text-foreground-neutral max-w-200 mx-auto">
+              Streamline your
+              <br /> development workflow
+            </h1>
+          </div>
           <p className="text-center text-foreground-neutral max-w-2xl mx-auto">
-            Real Postgres with the developer experience and infrastructure to
-            ship faster.
+            Prisma acts as your comprehensive enterprise data toolset,
+            simplifying database interactions and reducing complexity so
+            developers can focus on business logic.
           </p>
           <div className="flex flex-col md:flex-row gap-4 items-center justify-center">
             <Button
-              variant="ppg"
+              variant="orm"
               href="https://console.prisma.io/sign-up?utm_source=website&utm_medium=index&utm_campaign=cta"
               size="3xl"
               target="_blank"
               rel="noopener noreferrer"
               className="font-sans-display! font-[650]"
             >
-              <span>Create database</span>
-              <i className="fa-regular fa-database ml-2" />
+              <span>Get in touch</span>
+              <i className="fa-regular fa-envelope ml-2" />
             </Button>
-            <CopyCode text="npx prisma init">
-              <span className="text-foreground-neutral-reverse-weak">$</span>
-              <span className="text-foreground-neutral-weak">
-                &nbsp;npx prisma init
-              </span>
-              <i className="fa-regular fa-copy ml-2" />
-            </CopyCode>
           </div>
         </div>
       </div>
-      <div
-        className={cn(
-          "py-12 px-4 relative",
-          "before:absolute before:content-[''] before:inset-0 before:opacity-20 before:z-0",
-          "before:bg-[linear-gradient(180deg,var(--color-foreground-orm-weak)_0%,var(--color-background-default)_100%)]",
-        )}
-      >
-        <div className="max-w-[1200px] mx-auto z-1 relative flex flex-col gap-40">
-          <div className="cards mx-auto max-w-222 flex flex-col gap-6">
-            <h2 className="text-foreground-neutral text-center stretch-display text-4xl font-black! font-sans-display mt-0 mb-4">
-              Works with your favourite
-              <br />
-              databases and framework
-            </h2>
-            <Card className="bg-background-default flex-row! md:gap-8! justify-between items-center md:p-8 flex-wrap">
-              <h5 className="text-xl font-sans-display stretch-display font-bold md:w-min text-foreground-neutral">
-                {databases.title}
-              </h5>
-              <div className="flex gap-1 flex-wrap">
-                {databases.list.map((db) => (
-                  <Technology text={db.name} url={db.url}>
-                    <Action
-                      color="neutral"
-                      size="4xl"
-                      key={db.name}
-                      className="h-[75px]! w-[75px]! hover:bg-background-neutral-strong"
-                    >
-                      <img src={db.icon} alt={db.name} />
-                    </Action>
-                  </Technology>
-                ))}
-              </div>
-            </Card>
-            <Card className="bg-background-default md:gap-8! justify-between items-start md:items-center md:p-8">
-              <div className="flex gap-2 md:gap-6 items-start md:items-center md:flex-row flex-col">
-                <h5 className="text-xl font-sans-display stretch-display font-bold md:w-min text-foreground-neutral">
-                  {frameworks.title}
-                </h5>
-                <p className="text-base text-foreground-neutral-weak">
-                  {frameworks.description}
-                </p>
-              </div>
-              <div className="flex gap-1 flex-wrap">
-                {frameworks.list.map((fw) => (
-                  <Technology text={fw.name} url={fw.url}>
-                    <Action
-                      color="neutral"
-                      size="4xl"
-                      key={fw.name}
-                      className="h-[75px]! w-[75px]! hover:bg-background-neutral-strong"
-                    >
-                      <img src={fw.icon} alt={fw.name} />
-                    </Action>
-                  </Technology>
-                ))}
-              </div>
-            </Card>
-            <div className="flex gap-4 mx-auto w-fit flex-wrap justify-center">
-              <Button variant="default-stronger" size="3xl">
-                <span>Browse examples on GitHub</span>
-                <i className="fa-brands fa-github ml-2" />
-              </Button>
-              <Button variant="orm" size="3xl">
-                <span>Prisma in your stack</span>
-                <i className="fa-regular fa-arrow-right ml-2" />
-              </Button>
-            </div>
-          </div>
+      <div className="my-12 px-4">
+        <h5 className="pt-12 text-center text-foreground-orm-strong stretch-display text-base font-sans-display uppercase">
+          Trusted by teams at
+        </h5>
+        <LogoParade />
+      </div>
+      <div className="my-12 px-4">
+        <CardSection cardSection={first} />
+      </div>
+      <div className="my-12 px-4">
+        <div className="py-12 relative gap-8 flex flex-col">
+          <h3 className="text-center text-foreground-neutral stretch-display text-3xl stretch-display font-sans-display my-0">
+            Leave the database complexities to us
+          </h3>
+          <p className="text-center text-foreground-neutral max-w-xl mx-auto">
+            Focus on core competencies of your team, rather than building and
+            managing complex infrastructure components.
+          </p>
+        </div>
+        <div className="grid grid-cols-2 gap-6 max-w-230 mx-auto w-full">
+          {complexities.map((card: any, index: number) => {
+            const last = index === complexities.length - 1;
+            return (
+              <Card
+                key={card.title}
+                className={cn(
+                  "bg-[linear-gradient(180deg,var(--color-background-default)_0%,var(--color-background-orm)_262.5%)]",
+                  last && "col-span-2",
+                )}
+              >
+                <div className={cn(last && "grid grid-cols-2 gap-6")}>
+                  <div className="flex flex-col gap-4">
+                    <div className="flex gap-4 items-center">
+                      <Action color="orm" size="4xl">
+                        <i className={card.icon} />
+                      </Action>
+                      <h3 className="text-foreground-neutral font-sans-display text-xl stretch-display mt-0 mb-1 font-bold">
+                        {card.title}
+                      </h3>
+                    </div>
+                    <p className="text-foreground-neutral dark:text-foreground-neutral-weak text-sm font-normal m-0">
+                      {card.subtitle}
+                    </p>
+                    {!last && (
+                      <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[linear-gradient(0deg,var(--color-background-default)_0%,transparent_62.5%)] after:-bottom-4">
+                        <img
+                          src={`${card.image}.svg`}
+                          alt="Enterprise"
+                          className="hidden dark:block -mb-4"
+                        />
+                        <img
+                          src={`${card.image}_light.svg`}
+                          className="block dark:hidden -mb-4"
+                          alt="Enterprise"
+                        />
+                      </div>
+                    )}
+                  </div>
+                  {last && (
+                    <div className="flex gap-2 flex-col">
+                      <h5 className="text-xl font-sans-display stretch-display font-bold w-fit text-foreground-neutral">
+                        {databases.title}
+                      </h5>
+                      <div className="flex gap-1 flex-wrap">
+                        {databases.list.map((db) => (
+                          <Technology text={db.name} url={db.url} key={db.name}>
+                            <Action
+                              color="neutral"
+                              size="4xl"
+                              key={db.name}
+                              className="h-[75px]! w-[75px]! hover:bg-background-neutral-strong"
+                            >
+                              <img src={db.icon} alt={db.name} />
+                            </Action>
+                          </Technology>
+                        ))}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </Card>
+            );
+          })}
         </div>
       </div>
     </main>
---
+++ apps/site/src/components/animation.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { useRive, Layout, Alignment } from "@rive-app/react-webgl2";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+
+interface AnimationProps {
+  name: string;
+  className?: string;
+  fit?: reactCanvas.Fit;
+  threshold?: number;
+  style?: React.CSSProperties;
+  autoplay?: boolean;
+}
+
+export const Animation = ({
+  name,
+  className,
+  fit,
+  threshold,
+  style,
+  autoplay,
+}: AnimationProps) => {
+  const [isVisible, setVisible] = useState<boolean>(false);
+
+  const [reference, isInView] = useInView({
+    threshold: threshold ? threshold : 0.2,
+  });
+
+  const { rive, RiveComponent } = useRive({
+    src: `/animations/${name}.riv`,
+    autoplay: autoplay || false,
+    onLoad: () => console.log("Rive loaded successfully"),
+    onLoadError: (e) => console.error("Rive load error:", e),
+    layout: new Layout({
+      fit: fit,
+      alignment: Alignment.Center,
+    }),
+  });
+
+  useEffect(() => {
+    if (isInView) setVisible(true);
+    else setVisible(false);
+  }, [isInView]);
+
+  useEffect(() => {
+    if (rive) {
+      if (isVisible) {
+        rive.play();
+      } else rive.pause();
+    }
+    return () => rive?.pause(); // <- this
+  }, [isVisible, rive]);
+
+  return (
+    <div
+      ref={reference}
+      data-testid="rive-animation"
+      className={className}
+      {...{ style: style }}
+    >
+      <RiveComponent />
+    </div>
+  );
+};
