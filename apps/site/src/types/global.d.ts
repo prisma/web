d9b42753df4b9e4f651bb2d41744fd59922182ab
Carla Goncalves <goncalves@prisma.io>
2026-03-30 19:27:52 +0100

    Updateg

--- apps/site/src/types/global.d.ts
+++ apps/site/src/types/global.d.ts
@@ -2,11 +2,8 @@ declare global {
   interface Window {
     Tally?: {
       loadEmbeds: () => void;
-<<<<<<< feat/DR-7738-partners
       openPopup: (formId: string, options?: Record<string, unknown>) => void;
       closePopup: (formId: string) => void;
-=======
->>>>>>> main
     };
   }
 }
