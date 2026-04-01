6b1a3b2cb620bd694e1578451419962279a51dd8
Carla Goncalves <goncalves@prisma.io>
2026-04-01 15:35:00 +0100

    update how_section

--- apps/site/src/components/prisma-with/how-section.tsx
+++ apps/site/src/components/prisma-with/how-section.tsx
@@ -45,49 +45,48 @@ export async function HowSection({
         {hasTabs && (
           <div className="max-w-249 mx-auto rounded-xl w-full overflow-hidden border border-stroke-neutral">
             <Tabs defaultValue={data.tabs!.defaultValue} className="my-0">
-            <TabsList>
-              {data.tabs!.head.map((tab) => (
-                <TabsTrigger
-                  key={tab.value}
-                  className="py-3 text-foreground-neutral-weak data-[state=active]:text-foreground-ppg data-[state=active]:border-stroke-ppg data-[state=inactive]:font-normal!"
-                  value={tab.value}
-                >
-                  {tab.title}
-                </TabsTrigger>
-              ))}
-            </TabsList>
-            {await Promise.all(
-              data.tabs!.body.map(async (body) => {
-                return (
-                  <TabsContent key={body.value} value={body.value}>
-                    <div className="bg-[linear-gradient(0deg,var(--color-background-success)_0%,var(--color-background-default)_100%)] tabs-content p-4 border-t border-stroke-neutral [&>h4]:font-sans-display [&>h4]:text-foreground-neutral [&>h4]:text-xl [&>h4]:font-bold text-foreground-neutral-weak flex flex-col gap-6 pt-8">
-                      {parse(body.content)}
-                      {codeExamples[body.value] && (
-                        <div className="py-6 px-7 bg-background-neutral-weaker border rounded-[8px] overflow-hidden border-stroke-neutral [&>figure]:my-0 [&>figure>div]:py-0 [&>figure>div]:bg-transparent">
-                          <CodeBlock
-                            keepBackground={true}
-                            className="border-none [&_pre]:bg-transparent"
-                          >
-                            <div
-                              dangerouslySetInnerHTML={{
-                                __html: await prisma_highlighter.codeToHtml(
-                                  codeExamples[body.value],
-                                  {
+              <TabsList>
+                {data.tabs!.head.map((tab) => (
+                  <TabsTrigger
+                    key={tab.value}
+                    className="py-3 text-foreground-neutral-weak data-[state=active]:text-foreground-ppg data-[state=active]:border-stroke-ppg data-[state=inactive]:font-normal!"
+                    value={tab.value}
+                  >
+                    {tab.title}
+                  </TabsTrigger>
+                ))}
+              </TabsList>
+              {await Promise.all(
+                data.tabs!.body.map(async (body) => {
+                  return (
+                    <TabsContent key={body.value} value={body.value}>
+                      <div className="bg-[linear-gradient(0deg,var(--color-background-success)_0%,var(--color-background-default)_100%)] tabs-content p-4 border-t border-stroke-neutral [&>h4]:font-sans-display [&>h4]:text-foreground-neutral [&>h4]:text-xl [&>h4]:font-bold text-foreground-neutral-weak flex flex-col gap-6 pt-8">
+                        {parse(body.content)}
+                        {codeExamples[body.value] && (
+                          <div className="py-6 px-7 bg-background-neutral-weaker border rounded-[8px] overflow-hidden border-stroke-neutral [&>figure]:my-0 [&>figure>div]:py-0 [&>figure>div]:bg-transparent">
+                            <CodeBlock
+                              keepBackground={true}
+                              className="border-none [&_pre]:bg-transparent"
+                            >
+                              <div
+                                dangerouslySetInnerHTML={{
+                                  __html: await (
+                                    await prisma_highlighter()
+                                  ).codeToHtml(codeExamples[body.value], {
                                     lang: "typescript",
                                     theme: "prisma-dark",
-                                  },
-                                ),
-                              }}
-                            />
-                          </CodeBlock>
-                        </div>
-                      )}
-                    </div>
-                  </TabsContent>
-                );
-              }),
-            )}
-          </Tabs>
+                                  }),
+                                }}
+                              />
+                            </CodeBlock>
+                          </div>
+                        )}
+                      </div>
+                    </TabsContent>
+                  );
+                }),
+              )}
+            </Tabs>
           </div>
         )}
       </div>
