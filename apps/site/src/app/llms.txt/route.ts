4327b35eab3288953658de4a6e71bf7b26d469cb
Aidan McAlister <aidankmcalister@gmail.com>
2026-04-02 12:26:46 -0400

    fix(site): llms.txt and llms-full.txt not now working

--- apps/site/src/app/llms.txt/route.ts
+++ apps/site/src/app/llms.txt/route.ts
@@ -1,9 +1,9 @@
-import { buildLlmsFullContent } from "../llms-content";
+import { buildLlmsIndexContent } from "../llms-content";
 
 export const revalidate = false;
 
 export async function GET() {
-  return new Response(buildLlmsFullContent(), {
+  return new Response(buildLlmsIndexContent(), {
     headers: {
       "Content-Type": "text/plain; charset=utf-8",
     },
